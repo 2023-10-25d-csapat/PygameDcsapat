e1863d457eb34bdb5d83ea48f7932537321af2c5
nemethtamas <146165828+NemethTamasJedlik@users.noreply.github.com>
2023-12-14 10:09:05 +0100

    Update defekt.py

---
+++ defekt.py
@@ -0,0 +1,8 @@
+def megjelenítés(tszó, tpk):
+  megjelenítés = ''
+  for karakter in tszó:
+    if karakter in tpk:
+      megjelenítés += karakter
+    else:
+      megjelenítés += '_'
+  return megjelenítés
