99b3fccbcdcf290f91c714f860e3904be70cee24
kocsisjonatan <146165546+KocsisJonatan25@users.noreply.github.com>
2023-12-14 10:28:09 +0100

    második funkció meghívva

--- szorakoz.py
+++ szorakoz.py
@@ -7,14 +7,6 @@ tipp = []
 akarsz = input("Szeretnél Játszani ? (I/N)")
 if akarsz.lower() == "i" and akarsz.isalpha():
     print("=-=-=-=-=-= The Akasztófa =-=-=-=-=-=")
+    amig(akarsz, tipp, szó)
 else:
-   print("Ez van")
-
-while True:
-  if akarsz:
-    _1 =input("kaparj ide valamit: ")
-    tipp.append(_1)
-    print(megjelenítés(szó, tipp))
-    if megjelenítés(szó, tipp) == szó:
-      print("Gratulálok! Találtad a titkos szót:", szó)
-      break
+   print("(T_T)")
